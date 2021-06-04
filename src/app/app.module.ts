9d96619aa3f6b26e72759726c39af6a0db94be3d
Dante Pileckas <pileckas33@gmail.com>
2021-06-04 00:00:45 -0300

    Desing Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
@@ -18,7 +21,7 @@ import { APP_ROUTING } from './app.routes';
     ExperenceComponent,
     ProyectsComponent,
     ContactComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
