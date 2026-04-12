a65553d2afb199c610ca0600e57375e1900b4ad0
DantePileckas <pileckas33@gmail.com>
2026-04-12 16:01:31 -0300

    feat: add Google Analytics and Microsoft Clarity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent   {
+export class AppComponent {
   title = 'dantePileckas';
 
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd))
+      .subscribe((event) => {
+        gtag('config', 'G-N0F9J3CZ50', {
+          page_path: event.urlAfterRedirects
+        });
+      });
+  }
 }
